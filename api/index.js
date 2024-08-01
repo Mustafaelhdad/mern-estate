dff7430d24bc8d684a1d77c866af3533366fe70b
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2024-08-01 04:13:00 +0300

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRouter from "./routes/user-route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -12,9 +13,11 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
