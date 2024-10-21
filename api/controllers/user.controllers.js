4e2546da4c5e36dd15d97dc6768c99dc33c7c3c9
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2024-10-21 22:06:51 +0300

    create user listings api route

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user-model.js";
+import Listing from "../models/listing.modal.js";
 
 export const test = (req, res) => {
   res.json({
@@ -51,6 +52,19 @@ export const deleteUser = async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
 
     res.status(200).clearCookie("access_token").json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler((401, "You can only see your listings!")));
+
+  try {
+    const listings = await Listing.find({ userRef: req.params.id });
+
+    res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
