1c5e94f680713898e93e104d896d66c71f45a049
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2024-10-26 05:07:49 +0300

    setup for deployment

--- api/index.js
+++ api/index.js
@@ -8,12 +8,16 @@ import userRouter from "./routes/user-route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 
+import path from "path";
+
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then((res) => console.log("connected to MongoDB"))
   .catch((err) => console.log(err));
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(cookieParser());
@@ -36,6 +40,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
