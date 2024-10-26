115a0f5a91ea9de0e03cee1e73140a8a7b02f81d
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2024-10-26 05:21:47 +0300

    ..

--- api/index.js
+++ api/index.js
@@ -1,40 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-
-import userRouter from "./routes/user-route.js";
+import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
-
+import cookieParser from "cookie-parser";
 import path from "path";
-
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
-  .then((res) => console.log("connected to MongoDB"))
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const __dirname = path.resolve();
 
 const app = express();
-app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
-  console.log("server is running on port 3000");
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRouter);
@@ -50,7 +42,6 @@ app.get("*", (req, res) => {
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-
   return res.status(statusCode).json({
     success: false,
     statusCode,
